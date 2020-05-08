214c1e32ef5440c87ee609967be387bf95f088b8
cova <oscar_ecr1@hotmail.com>
2020-05-08 08:53:04 -0500

    yes

---
+++ main.cs
@@ -0,0 +1,29 @@
+using System;
+
+ 
+
+namespace Calculator
+
+{
+
+
+    class Operations
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+           
+        }
+
+        private int multiplicacion(int number1, int number2)
+        {
+            resultado = number1 * number2;
+            return resultado;
+        }
+
+    }
+
+}
