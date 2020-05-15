2dd4e4f61c11591c96ba05471cb74fea544937c1
Guillermo Ruvalcaba <guillermo8931@gmail.com>
2020-05-15 08:41:54 -0500

    Suma

--- main.cs
+++ main.cs
@@ -14,8 +14,21 @@ namespace Calculator
         static void Main(string[] args)
 
         {
+            Suma(){
+                int num1, num2, resultado;
 
-           
+                num1 = Convert.ToInt32(Console.ReadLine());
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                resultado = num1 + num2;
+
+                Console.WriteLine("El resultado es " + resultado);
+
+                Console.Read();
+            }
+
+
+
         }
 
 
